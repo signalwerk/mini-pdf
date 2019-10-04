621ca09521878eae5a87ffaa72a76471f0d1a061
Stefan Huber <sh@signalwerk.ch>
2019-10-04 11:59:09 +0200

    ADD: render example

---
+++ data/structure.js
@@ -0,0 +1,38 @@
+const DOC = {
+  type: "document",
+  attributes: {},
+  children: [
+    {
+      type: "viewport",
+      attributes: {
+        width: "",
+        height: ""
+      },
+      children: [
+        {
+          type: "text",
+          attributes: {
+            left: "",
+            right: "",
+            fontSize: "",
+            fontFamily: ""
+          },
+          content: "hello world"
+        },
+        {
+          type: "line",
+          attributes: {
+            x1: 0,
+            y1: 0,
+            x2: 0,
+            y2: 0,
+            stroke: "black",
+            strokeWidth: 1
+          },
+        }
+      ]
+    }
+  ]
+};
+
+export default DOC;
